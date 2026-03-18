a87089dd6a96a092c2644bca55c78f815abda9eb
Toddynh0BR <galaxyplay41@gmail.com>
2026-03-18 08:26:55 -0300

    Implementação da questão 8

--- Q8.py
+++ Q8.py
@@ -14,24 +14,26 @@
 #forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro
 #da gasolina é R$ 3,30 e o preço do litro do álcool é R$ 2,90.
 
-print('Digite o combustível desejado(A: Álcool, G: Gasolina): ')
+import time
+print('Digite o combustível desejado (A: Álcool, G: Gasolina): ')
 
 while True:
     tipo = input('Tipo: ').strip().upper()
 
     if tipo != 'A' and tipo != 'G':
-        print('Digite um tipo válido(A ou G).')
+        print('Digite um tipo válido (A para Álcool ou G para Gasolina).')
         continue
 
     valor = 2.9 if tipo == 'A' else 3.3
-    print('Combustivel escolhido:', 'Álcool' if tipo == 'A' else 'Gasolna')
+    print('Combustivel escolhido:', 'Álcool' if tipo == 'A' else 'Gasolina')
+    time.sleep(1)
     break
 
-print('Digite quantos litros você deseja: ')
+print('Digite quantos litros você deseja colocar: ')
 while True:
     litrosString = input('Litros: ').replace(',', '.').strip()
 
-    if not litrosString.replace('.', '.', 1).isdigit():
+    if not litrosString.replace('.', '', 1).isdigit():
        print('Digite um número válido.')
        continue
 
@@ -48,4 +50,25 @@ else:
 preco = (litros * valor)
 precoComDesconto = preco - (desconto * preco)
 
-print(f'Valor total: R${str(precoComDesconto).replace('.', ',')} Reais')
+#barra de carregamento para deixar mais interessante
+total = 25  #total de barrinhas que vão aparecer na barra de carregamento
+for i in range(total + 1):
+    #cria um loop de um 0 a 26(range nunca vai ate o numero maximo ex: 25 ele vai ate 24)
+
+    barra = "\\" * i
+    #multiplica as barras a cada for aumentando-as
+
+    print(f"\r⛽ Abastecendo {barra:<25} {i*4}%", end="")
+    #cria um print formated string a cada for com o numero de barras aumentado
+    #a cada for, o end="" impede que o codigo quebre a linha mantendo um print acima do outro
+    #
+    #senao seria algo como:
+    #Abastecendo //
+    #Abastecendo /////
+    #Abastecendo ////////
+    #Abastecendo ///////////
+
+    time.sleep(0.2)
+
+
+print(f'\n\nValor total: R${str(precoComDesconto).replace('.', ',')} Reais com {str(10 * desconto).replace('.', '').replace('0', '')}% de desconto.')
